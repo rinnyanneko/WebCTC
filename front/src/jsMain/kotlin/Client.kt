a4a60e001defae89f0f55697aa04bcf80699f7a5
Kaiz_JP <kaiz@kaiz.jp>
2024-04-03 06:07:26 +0900

    ルーティングを階層に

--- front/src/jsMain/kotlin/Client.kt
+++ front/src/jsMain/kotlin/Client.kt
@@ -1,4 +1,3 @@
-
 import io.ktor.client.*
 import io.ktor.client.engine.js.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -29,44 +28,51 @@ val client = HttpClient(Js) {
 fun main() {
     val container = document.createElement("div").also {
         document.body.appendChild(it)
-
-        document.body.style.apply {
-            fontFamily = "'Noto Sans JP', sans-serif"
-        }
+        document.body.style.apply { fontFamily = "'Noto Sans JP', sans-serif" }
     }
 
     val router = createBrowserRouter(arrayOf(
         jso {
             path = "/"
-            element = MapView.create()
-        }, jso {
-            path = "/p/account"
-            element = Account.create()
-        }, jso {
-            path = "/p/railgroup"
-            element = RailGroupManager.create()
-        }, jso {
-            path = "/p/waypoint"
-            element = WayPointEditor.create()
-        }, jso {
-            path = "/p/tecons"
-            element = TeConList.create()
-        }, jso {
-            path = "/p/tecons/view/:uuid"
-            element = TeConView.create()
-        }, jso {
-            path = "/p/tecons/edit/:uuid"
-            element = TeConEdit.create()
-        }, jso {
-            path = "/login"
-            element = Login.create()
+            children = arrayOf(
+                jso {
+                    index = true
+                    element = MapView.create()
+                }, jso {
+                    path = "login"
+                    element = Login.create()
+                }, jso {
+                    path = "p"
+                    children = arrayOf(
+                        jso {
+                            path = "account"
+                            element = Account.create()
+                        }, jso {
+                            path = "railgroup"
+                            element = RailGroupManager.create()
+                        }, jso {
+                            path = "waypoint"
+                            element = WayPointEditor.create()
+                        }, jso {
+                            path = "tecons"
+                            children = arrayOf(
+                                jso {
+                                    index = true
+                                    element = TeConList.create()
+                                }, jso {
+                                    path = "view/:uuid"
+                                    element = TeConView.create()
+                                }, jso {
+                                    path = "edit/:uuid"
+                                    element = TeConEdit.create()
+                                })
+                        })
+                })
         }, jso {
             path = "*"
             element = FallBack.create()
-        }
-    ))
+        })
+    )
 
-    createRoot(container).render(RouterProvider.create {
-        this.router = router
-    })
+    createRoot(container).render(RouterProvider.create { this.router = router })
 }
